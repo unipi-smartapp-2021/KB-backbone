64e4ce17badb67484933664384ab41152a32ce8d
Niccolò Piazzesi <npiazzesi@gmail.com>
2021-11-13 10:58:23 +0100

    inital project template

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ros:noetic-ros-base
+
+ENV ROS_WS=/home/ws
+
+RUN mkdir -p $ROS_WS/src
+
+COPY ./ $ROS_WS/src
+WORKDIR $ROS_WS
+
+RUN . "/opt/ros/$ROS_DISTRO/setup.sh" && cd $ROS_WS && catkin_make \
+  && sed --in-place --expression \ 
+  '$isource "$ROS_WS/devel/setup.bash"' \
+  /ros_entrypoint.sh
+
+CMD [ "roslaunch","backbone","backbone.launch" ]
---
+++ src/listener.cpp
@@ -0,0 +1,18 @@
+
+#include "ros/init.h"
+#include "ros/node_handle.h"
+#include "ros/ros.h"
+#include "ros/subscriber.h"
+#include "std_msgs/String.h"
+
+void callback(const std_msgs::String::ConstPtr &msg) {
+  ROS_INFO("Heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, callback);
+  ros::spin();
+  return 0;
+}
